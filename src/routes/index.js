d50695288a7a24e4403cbd83f4ad8429294b1953
Augusto Poloni <a_poloni@unoesc.edu.br>
2025-11-30 18:17:29 -0300

    Atualização

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,15 @@ const comandaController = require('../controllers/comandaController');
 const itemCardapioController = require('../controllers/itemCardapioController');
 const pedidoController = require('../controllers/pedidoController');
 const viewKDSController = require('../controllers/viewKDSController');
+const authController = require('../controllers/authController'); // <--- Importe aqui
+
+routes.post('/login', authController.login);
+routes.post('/register', authController.register);
+
 
 routes.post('/cardapio', itemCardapioController.store);
 routes.get('/cardapio', itemCardapioController.index);
+routes.delete('/cardapio/:id', itemCardapioController.delete);
 
 routes.post('/comandas', comandaController.abrir); 
 routes.put('/comandas/:id/fechar', comandaController.fechar);
@@ -16,9 +22,7 @@ routes.get('/comandas/:id', comandaController.show);
 routes.post('/comandas/:id/adicionar', pedidoController.adicionarItem);
 
 routes.get('/kds/cozinha', viewKDSController.getCozinha);
-
 routes.get('/kds/copa', viewKDSController.getCopa);
-
 routes.put('/pedidos/:itemId/status', pedidoController.atualizarStatus);
 
 module.exports = routes;
